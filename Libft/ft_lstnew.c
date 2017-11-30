1a32db9c20bd15589689252cd7296481ef7347f8
Arthur LANTER <alanter@e2r6p5.42.fr>
2017-11-30 16:59:26 +0100

    Bonus functions 1

--- Libft/ft_lstnew.c
+++ Libft/ft_lstnew.c
@@ -6,23 +6,33 @@
 /*   By: alanter <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/29 22:36:34 by alanter           #+#    #+#             */
-/*   Updated: 2017/11/30 00:13:52 by alanter          ###   ########.fr       */
+/*   Updated: 2017/11/30 16:56:21 by alanter          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-//pourquoi free ?
 t_list	*ft_lstnew(void const *content, size_t content_size)
 {
-	t_list *lstnew;
-	
+	t_list	*lstnew;
+
+	if (!(lstnew = (t_list*)malloc(sizeof(t_list))))
+		return (NULL);
 	if (content == NULL)
+	{
 		lstnew->content = NULL;
+		lstnew->content_size = 0;
+	}
 	else
-		if(!(lstnew->content = (content*)malloc(sizeof(content*) * content_size))
-				return (NULL)
-	lstnew.content_size = 0;
+	{
+		if (!(lstnew->content = (void*)malloc(content_size)))
+		{
+			free(lstnew);
+			return (NULL);
+		}
+		lstnew->content = (void *)content;
+		lstnew->content_size = content_size;
+	}
 	lstnew->next = NULL;
 	return (lstnew);
 }
